bc22c28a7b8ce70029db86211f8d186b94031bfb
Bug520-Maker <betweent@126.com>
2021-05-15 09:44:09 +0800

    完善聊天

--- src/components/common/navBarControl/NavBarControl.vue
+++ src/components/common/navBarControl/NavBarControl.vue
@@ -1,6 +1,20 @@
 <template>
   <div class="bav-bar-control">
-
+    <ul class="nav-list" :style="{width:listStyle.width,background:listStyle.background,height:listStyle.height}">
+      <li v-for="(item,index) in list"
+          :key="item.user.userId" @click="liClick(item,index)"
+          :class="{active:currentIndex===index}">
+        <div class="avatar" v-if="item.user.userId!==currentUser">
+          <img :src="item.user.avatarUrl" />
+        </div>
+        <div class="user-name" v-if="item.user.userId!==currentUser">
+          {{item.user.userName}}
+        </div>
+      </li>
+    </ul>
+    <div v-for="(item,index) in list" v-if="currentIndex===index" class="nav-control-content">
+      <slot :name="item.user.userId"></slot>
+    </div>
   </div>
 </template>
 
@@ -14,11 +28,78 @@ export default {
       {
         return []
       }
+    },
+    listStyle:{
+      type:Object,
+      default()
+      {
+        return {
+          width:"150px",
+          background:"#89bbf0",
+          height:"550px"
+        }
+      }
+    },
+    currentUser:{
+      type:String,
+      default:''
+    }
+  },
+  data()
+  {
+    return {
+      currentIndex:0
+    }
+  },
+  methods:{
+    liClick(item,index)
+    {
+      this.currentIndex=index;
+      this.$emit("currentChat",item)
     }
   }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="less">
+  .center()
+  {
+    position: absolute;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+  }
+  .bav-bar-control{
+    display: flex;
+    align-items: flex-start;
+    .nav-control-content{
+      flex: 1;
+    }
+  }
+  .nav-list{
+    li{
+      cursor: pointer;
+      padding:5px;
+      &.active{
+        background-color: rgba(0,0,0,.1);
+      }
+      display: flex;
+      align-items: center;
+      .avatar{
+        width: 30px;
+        height: 30px;
+        overflow: hidden;
+        position: relative;
+        border-radius: 50%;
+        img{
+          .center();
+          height: 100%;
+        }
+      }
+      .user-name{
+        font-size: 13px;
+        margin: 0 0 0 5px;
+      }
+    }
+  }
 </style>
--- src/components/content/chat/Chat.vue
+++ src/components/content/chat/Chat.vue
@@ -4,35 +4,41 @@
       {{userDetail.userName}}
     </div>
     <div class="body">
-      <div class="user-list"></div>
-      <div class="content">
-        <div class="content-text">
-          <ul class="user-message" ref="content">
-            <li class="profile" v-for="(item,index) in contentText" :key="item.id" >
-              <div class="msg" v-if="item.user.userId===store.state.userMsg.userId||item.user.userId===userId"
-                               :class="{active:item.user.userId===userId}">
-                <span class="bubble">{{item.content}}</span>
-                <div class="avatar" v-if="item.user">
-                  <img :src="item.user.avatarUrl" />
-                </div>
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div class="inp">
-          <textarea cols="124" rows="4" v-model="content"></textarea>
-          <button class="send" @click="send">发送</button>
-        </div>
+        <nav-bar-control :list="chatUsers" v-if="chatUsers.length!==0" @currentChat="currentChat"
+                         :current-user="store.state.userMsg.userId">
+          <div class="content" v-for="(item,index) in chatUsers" :slot="item.user.userId">
+            <div class="content-text">
+              <!--聊天记录-->
+              <ul class="user-message">
+                <li class="profile" v-for="(item,index) in contentText" :key="item.id" >
+                  <div class="msg" v-if="item.user.userId===store.state.userMsg.userId||item.user.userId===userId">
+                    <div v-for="(it,i) in item.content" key="i" style="display: flex" :class="{active:item.user.userId===userId}">
+                      <span class="bubble">{{it}}</span>
+                      <div class="avatar" v-if="item.user">
+                        <img :src="item.user.avatarUrl" alt="暂无头像" />
+                      </div>
+                    </div>
+                  </div>
+                </li>
+              </ul>
+            </div>
+            <div class="inp">
+              <textarea cols="124" rows="4" v-model="content"></textarea>
+              <button class="send" @click="send">发送</button>
+            </div>
+          </div>
+        </nav-bar-control>
       </div>
     </div>
-  </div>
 </template>
 
 <script>
 import {userDetail} from "@/network/user";
 import store from "../../../store"
+import NavBarControl from "@/components/common/navBarControl/NavBarControl";
 export default {
   name: "Chat",
+  components: {NavBarControl},
   data()
   {
     return {
@@ -46,7 +52,8 @@ export default {
       //当前用户
       currentUser:null,
       //在线用户
-      chatUsers:[]
+      chatUsers:[],
+      isCurrentUser:true
     }
   },
   created() {
@@ -55,27 +62,52 @@ export default {
     userDetail(this.userId).then(data=>{
       console.log(data);
       this.userDetail=data;
+
+      this.chatUsers.push({
+        content:'',
+        user:{
+          userId:this.userId,
+          userName:this.userDetail.userName,
+          avatarUrl: this.userDetail.avatarUrl
+        }
+      })
     })
     this.currentUser={
       userId:this.userId
     }
-    this.chatUsers.push({
-      userId:this.userId
-    })
+
     this.chat();
+  },
+  computed:{
+    currentContent()
+    {
+
+    }
   },
   methods:{
     send()
     {
        let content={
-         id:new Date().getTime(),
-         content:this.content,
+         content:[this.content],
          user:{
+           id:new Date().getTime(),
            avatarUrl:this.$store.state.userMsg.avatarUrl,
            userId:this.$store.state.userMsg.userId
          }
        }
-       this.contentText.push(content);
+       const isExists=this.contentText.findIndex((item,index)=>{
+         console.log(this.$store.state.userMsg.userId);
+         console.log(item.user.userId)
+         return item.user.userId===this.$store.state.userMsg.userId
+       })
+      console.log(isExists);
+      if(isExists===-1)
+      {
+        this.contentText.push(content);
+      }
+      else if(isExists!==-1){
+        this.contentText[isExists].content.push(this.content);
+      }
        this.sendMessage(this.content);
        this.content="";
     },
@@ -93,19 +125,57 @@ export default {
     {
       const {
         content,user
-      }=JSON.parse(msg.data)
-      this.message={
-        id:new Date().getTime(),
-        content:content,
-        user:user
+      }=JSON.parse(msg.data);
+      //不是当前聊天用户
+      if(user.userId!==this.userId)
+      {
+        let message=[];
+        const isExists=this.chatUsers.findIndex((item,index)=>{
+          return item.user.userId===user.userId
+        })
+        if(isExists!==-1)
+        {
+          this.chatUsers[isExists].content.push(content);
+        }
+        else if(isExists===-1)
+        {
+          message.push(content);
+          this.chatUsers.push({
+            user:user,
+            content:message
+          })
+        }
       }
-      console.log(this.message);
-      this.contentText.push(this.message);
-      console.log(JSON.parse(msg.data));
+      //是当前聊天用户
+      else{
+        this.message={
+          id:new Date().getTime(),
+          content:[content],
+          user:user
+        }
+        const isExists=this.contentText.findIndex((item,index)=>{
+          return item.user.userId===user.userId
+        })
+        if(isExists===-1)
+        {
+          this.contentText.push(this.message);
+        }else if(isExists!==-1){
+          this.contentText[isExists].content.push(content);
+        }
+      }
     },
     sendMessage(content)
     {
       this.socket.send(content);
+    },
+    //更改当前用户
+    currentChat(item)
+    {
+      this.socket.close();
+      this.userId=item.user.userId;
+      this.chat();
+      this.contentText=this.chatUsers;
+      console.log(item);
     }
   }
 }
@@ -136,7 +206,7 @@ export default {
         height: 550px;
       }
       .content{
-        flex: 1;
+        width: 840px;
         .content-text{
           padding: 10px 20px;
           height: 410px;
@@ -147,35 +217,37 @@ export default {
           .user-message{
             li{
               .msg{
-                padding: 8px 0;
-                display: flex;
-                align-items: center;
-                justify-content: flex-end;
-                .bubble{
-                  background-color: #12b7f5;
-                  font-size: 13px;
-                  color: #fff;
-                  padding: 5px 10px;
-                  border-radius: 10px;
-                }
-                .avatar{
-                  width: 30px;
-                  height: 30px;
-                  background-color: #111;
-                  margin: 0 0 0 15px;
-                  border-radius: 50%;
-                  position: relative;
-                  overflow:hidden;
-                  img{
-                    height: 100%;
-                    .center();
+                &>div{
+                  padding: 8px 0;
+                  display: flex;
+                  align-items: center;
+                  justify-content: flex-end;
+                  .bubble{
+                    background-color: #12b7f5;
+                    font-size: 13px;
+                    color: #fff;
+                    padding: 5px 10px;
+                    border-radius: 10px;
                   }
-                }
-                &.active{
-                  flex-direction: row-reverse;
-                  justify-content:flex-end;
                   .avatar{
-                    margin: 0 15px 0 0;
+                    width: 30px;
+                    height: 30px;
+                    background-color: #111;
+                    margin: 0 0 0 15px;
+                    border-radius: 50%;
+                    position: relative;
+                    overflow:hidden;
+                    img{
+                      height: 100%;
+                      .center();
+                    }
+                  }
+                  &.active{
+                    flex-direction: row-reverse;
+                    justify-content:flex-end;
+                    .avatar{
+                      margin: 0 15px 0 0;
+                    }
                   }
                 }
               }
